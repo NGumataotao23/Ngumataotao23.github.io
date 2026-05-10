8aa6cd50ed759b126db175116e29b856be3013b7
NGumataotao23 <ngumataotao23@gmail.com>
2026-05-10 14:06:31 -0700

    up7

--- switch.js
+++ switch.js
@@ -2,15 +2,18 @@ function showWork(which){
     const profwork = document.getElementById("Professional");
     const perswork = document.getElementById("Personal");
     const perswork2 = document.getElementById("Personal2");
+    const perswork3 = document.getElementById("Personal3");
 
     if(which == "pers"){
         perswork.style.display = "flex";
         perswork2.style.display = "flex";
+        perswork3.style.display = "flex";
         profwork.style.display = "none";
     }
     else{
         perswork.style.display = "none";
         perswork2.style.display = "none";
+        perswork3.style.display = "none";
         profwork.style.display = "flex";
     }
 
